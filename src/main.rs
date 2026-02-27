fa2476507c404148c096c14f26c554c2e2491e02
Dorian Niemiec <dorian@ferron.sh>
2026-02-27 14:16:14 +0100

    chore: update example usage in `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,19 @@ mod net;
 mod op;
 mod task;
 
-use std::net::ToSocketAddrs;
-
-use executor::new_runtime;
-
-use crate::{driver::AnyDriver, executor::spawn, io::AsyncWrite};
+use crate::{
+    driver::AnyDriver,
+    executor::{new_runtime, spawn},
+    io::AsyncWrite,
+};
 
 fn main() {
     let runtime =
-        new_runtime(AnyDriver::new_best().expect("failed to initialize runtime I/O driver"));
+        new_runtime(AnyDriver::new_mio().expect("failed to initialize runtime I/O driver"));
 
     // A basic TCP echo server example
     runtime.block_on(async {
-        let mut listener =
-            net::TcpListener::bind("127.0.0.1:5555".to_socket_addrs().unwrap().next().unwrap())
-                .unwrap();
+        let mut listener = net::TcpListener::bind("127.0.0.1:5555").unwrap();
         while let Ok((stream, _)) = listener.accept().await {
             spawn(async move {
                 let mut stream = stream;
